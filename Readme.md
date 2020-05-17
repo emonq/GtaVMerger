70f84fa35074b90f8fb03ec236773e8feb2e84d0
emonq <emonq@outlook.com>
2020-05-17 14:06:22 +0800

    great improvement

--- Readme.md
+++ Readme.md
@@ -6,13 +6,15 @@
 
 ## 2. 要怎么用?
 
-修改gtav.ps1脚本前三行的路径后运行
+~~修改gtav.ps1脚本前三行的路径后运行~~
 
-> $gtavDir 是你想要存放共享文件夹的目录, 合并后在该目录下会生成 Epic, Steam, General 三个文件夹
+> ~~$gtavDir 是你想要存放共享文件夹的目录, 合并后在该目录下会生成 Epic, Steam, General 三个文件夹~~
 >
-> $steam 是你现在存放steam的gta5文件的路径
+> ~~$steam 是你现在存放steam的gta5文件的路径~~
 >
-> $epic 是你现在存放epic的gta5文件的路径
+> ~~$epic 是你现在存放epic的gta5文件的路径~~
+
+直接运行, 按照提示内容输入路径
 
 如果你选择的路径需要较高权限 (如Program Files), 请使用管理员权限运行
 
--- gtav.ps1
+++ gtav.ps1
@@ -1,54 +1,63 @@
-$gtavDir="D:\Game Files\GTAV"
-$steam="D:\SteamLibrary\steamapps\common\Grand Theft Auto V"
-$epic="D:\Epic Games\GTAV"
+do {
+    $gtavDir = Read-Host "输入你想存放合并后游戏文件的位置"
+}while ($gtavDir -eq "")
+$steam = Read-Host "输入steam的gta文件夹位置,如果没有直接回车"
+$epic = Read-Host "输入epic的gta文件夹位置,如果没有直接回车"
 
-$epicDir=Join-Path $gtavDir "Epic"
-$steamDir=Join-Path $gtavDir "Steam"
-$generalDir=Join-Path $gtavDir "General"
+if (($steam -eq "") -and ($epic -eq "")) { Write-Host "不可以两个文件夹都不存在!`n按任意键退出" -ForegroundColor Red; [Console]::Readkey() | Out-Null; Exit }
+if ($steam -ne "") { $generalSrc = $steam }
+Else { $generalSrc = $epic }
 
-$steamDirs="Installers"
-$steamFiles="commandline.txt","GTAVLanguageSelect.exe","GTAVLauncher.exe","GTA5.exe","installscript.vdf","PlayGTAV.exe","steam_api64.dll"
+$epicDir = Join-Path $gtavDir "Epic"
+$steamDir = Join-Path $gtavDir "Steam"
+$generalDir = Join-Path $gtavDir "General"
 
-$epicDirs=".egstore","ReadMe","Redistributables","x64\data"
-$epicFiles="update\x64\data\errorcodes\chinesesimp.txt","EOSSDK-Win64-Shipping.dll","GPUPerfAPIDX11-x64.dll","GTA5.exe","NvPmApi.Core.win64.dll","PlayGTAV.exe","version.txt"
+$steamDirs = "Installers"
+$steamFiles = "commandline.txt", "GTAVLanguageSelect.exe", "GTAVLauncher.exe", "GTA5.exe", "installscript.vdf", "PlayGTAV.exe", "steam_api64.dll"
 
-$generalFiles="update\update.rpf","update\x64\data\errorcodes\american.txt","update\x64\data\errorcodes\chinese.txt","update\x64\data\errorcodes\french.txt","update\x64\data\errorcodes\german.txt","update\x64\data\errorcodes\italian.txt","update\x64\data\errorcodes\japanese.txt","update\x64\data\errorcodes\korean.txt","update\x64\data\errorcodes\mexican.txt","update\x64\data\errorcodes\polish.txt","update\x64\data\errorcodes\portuguese.txt","update\x64\data\errorcodes\russian.txt","update\x64\data\errorcodes\spanish.txt","x64\metadata.dat","bink2w64.dll","common.rpf","d3dcompiler_46.dll","d3dcsx_46.dll","GFSDK_ShadowLib.win64.dll","GFSDK_TXAA.win64.dll","GFSDK_TXAA_AlphaResolve.win64.dll","x64a.rpf","x64b.rpf","x64c.rpf","x64d.rpf","x64e.rpf","x64f.rpf","x64g.rpf","x64h.rpf","x64i.rpf","x64j.rpf","x64k.rpf","x64l.rpf","x64m.rpf","x64n.rpf","x64o.rpf","x64p.rpf","x64q.rpf","x64r.rpf","x64s.rpf","x64t.rpf","x64u.rpf","x64v.rpf","x64w.rpf"
-$generalDirs="x64\audio","update\x64\dlcpacks"
+$epicDirs = ".egstore", "ReadMe", "Redistributables", "x64\data"
+$epicFiles = "update\x64\data\errorcodes\chinesesimp.txt", "EOSSDK-Win64-Shipping.dll", "GPUPerfAPIDX11-x64.dll", "GTA5.exe", "NvPmApi.Core.win64.dll", "PlayGTAV.exe", "version.txt"
 
-if(-not (Test-Path -Path $epicDir)){mkdir -p $epicDir}
-if(-not (Test-Path -Path $steamDir)){mkdir -p $steamDir}
-if(-not (Test-Path -Path $generalDir)){mkdir -p $generalDir}
+$generalFiles = "update\update.rpf", "update\x64\data\errorcodes\american.txt", "update\x64\data\errorcodes\chinese.txt", "update\x64\data\errorcodes\french.txt", "update\x64\data\errorcodes\german.txt", "update\x64\data\errorcodes\italian.txt", "update\x64\data\errorcodes\japanese.txt", "update\x64\data\errorcodes\korean.txt", "update\x64\data\errorcodes\mexican.txt", "update\x64\data\errorcodes\polish.txt", "update\x64\data\errorcodes\portuguese.txt", "update\x64\data\errorcodes\russian.txt", "update\x64\data\errorcodes\spanish.txt", "x64\metadata.dat", "bink2w64.dll", "common.rpf", "d3dcompiler_46.dll", "d3dcsx_46.dll", "GFSDK_ShadowLib.win64.dll", "GFSDK_TXAA.win64.dll", "GFSDK_TXAA_AlphaResolve.win64.dll", "x64a.rpf", "x64b.rpf", "x64c.rpf", "x64d.rpf", "x64e.rpf", "x64f.rpf", "x64g.rpf", "x64h.rpf", "x64i.rpf", "x64j.rpf", "x64k.rpf", "x64l.rpf", "x64m.rpf", "x64n.rpf", "x64o.rpf", "x64p.rpf", "x64q.rpf", "x64r.rpf", "x64s.rpf", "x64t.rpf", "x64u.rpf", "x64v.rpf", "x64w.rpf"
+$generalDirs = "x64\audio", "update\x64\dlcpacks"
 
-if(-not (Test-Path -Path (Join-Path $steamDir "update\x64\data\errorcodes"))){mkdir -p (Join-Path $steamDir "update\x64\data\errorcodes")}
-if(-not (Test-Path -Path (Join-Path $steamDir "x64"))){mkdir -p (Join-Path $steamDir "x64")}
-if(-not (Test-Path -Path (Join-Path $epicDir "update\x64\data\errorcodes"))){mkdir -p (Join-Path $epicDir "update\x64\data\errorcodes")}
-if(-not (Test-Path -Path (Join-Path $epicDir "x64"))){mkdir -p (Join-Path $epicDir "x64")}
-if(-not (Test-Path -Path (Join-Path $generalDir "update\x64\data\errorcodes"))){mkdir -p (Join-Path $generalDir "update\x64\data\errorcodes")}
-if(-not (Test-Path -Path (Join-Path $generalDir "x64"))){mkdir -p (Join-Path $generalDir "x64")}
+if (-not (Test-Path -Path (Join-Path $steamDir "update\x64\data\errorcodes"))) { mkdir -p (Join-Path $steamDir "update\x64\data\errorcodes") }
+if (-not (Test-Path -Path (Join-Path $steamDir "x64"))) { mkdir -p (Join-Path $steamDir "x64") }
+if (-not (Test-Path -Path (Join-Path $epicDir "update\x64\data\errorcodes"))) { mkdir -p (Join-Path $epicDir "update\x64\data\errorcodes") }
+if (-not (Test-Path -Path (Join-Path $epicDir "x64"))) { mkdir -p (Join-Path $epicDir "x64") }
+if (-not (Test-Path -Path (Join-Path $generalDir "update\x64\data\errorcodes"))) { mkdir -p (Join-Path $generalDir "update\x64\data\errorcodes") }
+if (-not (Test-Path -Path (Join-Path $generalDir "x64"))) { mkdir -p (Join-Path $generalDir "x64") }
 
-Foreach($item in $epicDirs){Copy-Item (Join-Path $epic $item) (Join-Path $epicDir $item) -recurse}
-Foreach($item in $steamDirs){Copy-Item (Join-Path $steam $item) (Join-Path $steamDir $item) -recurse}
-Foreach($item in $epicFiles){Copy-Item (Join-Path $epic $item) (Join-Path $epicDir $item)}
-Foreach($item in $steamFiles){Copy-Item (Join-Path $steam $item) (Join-Path $steamDir $item)}
+if ($steam -ne "") {
+    Foreach ($item in $steamFiles) { Copy-Item (Join-Path $steam $item) (Join-Path $steamDir $item) }
+    Foreach ($item in $steamDirs) { Copy-Item (Join-Path $steam $item) (Join-Path $steamDir $item) -recurse }
+}
+if ($epic -ne ""){
+    Foreach ($item in $epicDirs) { Copy-Item (Join-Path $epic $item) (Join-Path $epicDir $item) -recurse }
+    Foreach ($item in $epicFiles) { Copy-Item (Join-Path $epic $item) (Join-Path $epicDir $item) }
+}
 
 Foreach ($item in $generalFiles) {
-    Copy-Item (Join-Path $epic $item) (Join-Path $generalDir $item)
-    if(Test-Path -Path (Join-Path $epicDir $item)){cmd /c rmdir (Join-Path $epicDir $item)}
-    if(Test-Path -Path (Join-Path $steamDir $item)){cmd /c rmdir (Join-Path $steamDir $item)}
+    Copy-Item (Join-Path $generalSrc $item) (Join-Path $generalDir $item)
+    if (Test-Path -Path (Join-Path $epicDir $item)) { cmd /c rmdir (Join-Path $epicDir $item) }
+    if (Test-Path -Path (Join-Path $steamDir $item)) { cmd /c rmdir (Join-Path $steamDir $item) }
     cmd /c mklink /H (Join-Path $epicDir $item) (Join-Path $generalDir $item)
     cmd /c mklink /H (Join-Path $steamDir $item) (Join-Path $generalDir $item)
 }
 Foreach ($item in $generalDirs) {
     Copy-Item (Join-Path $epic $item) (Join-Path $generalDir $item) -recurse
-    if(Test-Path -Path (Join-Path $epicDir $item)){cmd /c rmdir (Join-Path $epicDir $item)}
-    if(Test-Path -Path (Join-Path $steamDir $item)){cmd /c rmdir (Join-Path $steamDir $item)}
+    if (Test-Path -Path (Join-Path $epicDir $item)) { cmd /c rmdir (Join-Path $epicDir $item) }
+    if (Test-Path -Path (Join-Path $steamDir $item)) { cmd /c rmdir (Join-Path $steamDir $item) }
     cmd /c mklink /j (Join-Path $epicDir $item) (Join-Path $generalDir $item)
     cmd /c mklink /j (Join-Path $steamDir $item) (Join-Path $generalDir $item)
 }
-Rename-Item -Path $steam $steam+"_old"
-Rename-Item -Path $epic $epic+"_old"
-cmd /c mklink /j $steam $steamDir
-cmd /c mklink /j $epic $epicDir
-"Merge completed! You need to remove the old files manually."
-[Console]::Readkey() | Out-Null
-Exit
+if ($steam -ne ""){
+    Rename-Item -Path $steam $steam"_old"
+    cmd /c mklink /j $steam $steamDir
+}
+if($epic -ne ""){
+    Rename-Item -Path $epic $epic"_old"
+    cmd /c mklink /j $epic $epicDir
+}
+Write-Host "合并完成, 旧文件夹已被命名为原文件夹名_old, 请在测试游戏正常运行后手动删除`n按任意键退出" -ForegroundColor Green
+[Console]::Readkey() | Out-Null; Exit
